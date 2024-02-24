283922d2babe52a66c0580d97e89890b20916fe8
David <okekedavid1333@gmail.com>
2024-02-24 12:18:17 +0100

    update 2

--- js/update.js
+++ js/update.js
@@ -1,3 +1,38 @@
+let data = [
+    {
+        name: "School Bag",
+        price: 10,
+        quantity: 2,
+    },
+    {
+        name: "Tennis Racket",
+        price: 300,
+        quantity: 4,
+    },
+    {
+        name: "School Socks",
+        price: 1,
+        quantity: 1,
+    }
+]
+
+function displayItems() {
+    let cart =  document.querySelector('.cart')
+    cart.innerHTML = data.map((item) =>{
+        return`
+        <div class="item">
+        <h1>${item.name}</h1>
+        <p>$<span class="price">${item.price}</span></p>
+        <button class="inc">+</button>
+        <span class="qty">${item.quantity}</span>
+        <button class="dec">-</button>
+        <p class="total"></p>
+    </div>
+        `
+    })
+    
+}
+displayItems()
 let items = document.querySelectorAll('.item')
 
 items.forEach(item => {
@@ -19,3 +54,4 @@ items.forEach(item => {
         total.innerHTML = `$${price*quantity}`
     })
 });
+
